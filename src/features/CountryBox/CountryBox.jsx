ea57227a79fdaad2af3d8388553359d5b08969ba
Jaison Schmidt <jaison.schmidt@compasso.com.br>
2019-11-25 17:01:37 -0300

    CountryBox - doing

--- src/features/CountryBox/CountryBox.jsx
+++ src/features/CountryBox/CountryBox.jsx
@@ -3,9 +3,16 @@ import { CountryBoxWrap } from "./CountryBoxStyles";
 
 const CountryBox = props => {
   const {
-    country: { name }
+    country: { name, flag }
   } = props;
-  return <CountryBoxWrap>{name}</CountryBoxWrap>;
+  return (
+    <CountryBoxWrap>
+      <div className="image-wrap">
+        <img src={flag} alt="" />
+      </div>
+      {name}
+    </CountryBoxWrap>
+  );
 };
 
 export default CountryBox;
--- src/features/CountryBox/CountryBoxStyles.js
+++ src/features/CountryBox/CountryBoxStyles.js
@@ -2,9 +2,25 @@ import styled from "styled-components";
 import media from "styled-media-query";
 
 export const CountryBoxWrap = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
 
   ${media.greaterThan("medium")`
     width: 25%;
   `}
+
+  > .image-wrap {
+    height: 160px;
+    overflow: hidden;
+
+    img {
+      height: 100%;
+      object-fit: cover;
+      width: 265px;
+    }
+  }
+
+  > .country-name {
+  }
 `;

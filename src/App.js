86e631cf338da780656e6d611652689d052f8c74
Jaison Schmidt <jaison.schmidt@compasso.com.br>
2019-11-25 17:58:55 -0300

    Adicionado TODO

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
-import { Config } from "./config"
-import { ThemeProvider } from "styled-components"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import { CountryService } from './services/CountryService'
+import { Config } from "./config";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { CountryService } from "./services/CountryService";
 
 // redux
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux';
-import { loadCountries } from './redux/actions/actions';
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { loadCountries } from "./redux/actions/actions";
+
+// TODO melhorar responsividade dos boxes
+// TODO adicionar button de favorito no box
+// TODO criar página de country detail
+// TODO criar ação de adicionar a favoritos
+// TODO verificar se o country esta nos favoritos (box)
+// TODO verificar se o country esta nos favoritos (country detail)
+// TODO criar tela de countries favoritos
+// TODO criar button de skin
 
 /* Pages */
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
 
 /* theme */
-import ThemeDark from "./assets/styles/ThemeDark"
+import ThemeDark from "./assets/styles/ThemeDark";
 
 /* CSS Global */
-import GlobalStyles from "./assets/styles/Global"
+import GlobalStyles from "./assets/styles/Global";
 
 /* theme options */
 let Theme = [
@@ -25,28 +34,27 @@ let Theme = [
     title: "Dark",
     scheme: ThemeDark
   }
-]
+];
 
-let ThemeActive = 0
+let ThemeActive = 0;
 
 const App = props => {
-
   const { loadCountries } = props;
 
   // hook
   useEffect(() => {
     // Load countries
     const countryService = new CountryService();
-    
-    countryService
-      .getAllCountriesFromApi()
-      .then(res => {
-        console.log(res.data)
-        loadCountries(res.data)
-      })
-    
-    return () => { console.log('Destroy App'); }
-  }, [loadCountries])
+
+    countryService.getAllCountriesFromApi().then(res => {
+      console.log(res.data);
+      loadCountries(res.data);
+    });
+
+    return () => {
+      console.log("Destroy App");
+    };
+  }, [loadCountries]);
 
   document.title = Config.PAGE_TITLE;
   return (
@@ -63,9 +71,10 @@ const App = props => {
       </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-const mapDispatchToProps = dispatch => bindActionCreators({ loadCountries }, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ loadCountries }, dispatch);
 
 export default connect(null, mapDispatchToProps)(App);
